cda678975b6f1eeb640fbe6cad5944c891808921
59011-07-10 <59011-07-10@exchange.ad.afpanet>
2021-10-25 17:30:47 +0200

    25/10/2021 17:30:46,55

--- csharp/decouverte/decouverte/Program.cs
+++ csharp/decouverte/decouverte/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 
-namespace decouverte
+namespace voiture
 {
     class Program
     {
+        int couleur,marque,modele,nbde kilometres, motorisation;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
