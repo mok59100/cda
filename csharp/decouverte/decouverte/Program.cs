50db7332ed07224d16f75c39a48b68fea9f7b911
59011-07-10 <59011-07-10@exchange.ad.afpanet>
2021-10-26 12:20:53 +0200

    26/10/2021 12:20:50,30

--- csharp/decouverte/decouverte/Program.cs
+++ csharp/decouverte/decouverte/Program.cs
@@ -4,10 +4,26 @@ namespace voiture
 {
     class Program
     {
-        int couleur,marque,modele,nbde kilometres, motorisation;
         static void Main(string[] args)
+       
+            
         {
-            Console.WriteLine("Hello World!");
+            /*Exercice 1 - Saisie d’une chaîne
+Ecrire un programme demandant à l’utilisateur de saisir son nom, puis affichant le nom saisi.*/
+
+
+            /*Console.WriteLine("entre ton nom");
+            string nom = Console.ReadLine();
+       
+            Console.WriteLine("ton nom est " + nom );*/
+
+           /* Exercice 2 - Saisie d’un entier
+Ecrire un programme demandant à l’utilisateur de saisir une valeur numérique entière puis
+affichant cette valeur.*/
+
+            Console.WriteLine("saisi un entier");
+            string nombre = (Console.ReadLine);
+            Console.WriteLine("le chiffre est " + nombre);
         }
     }
 }
---
+++ csharp/exercice/exercice/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace exercice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Exercice 1 - Saisie d’une chaîne
+            Ecrire un programme demandant à l’utilisateur de saisir son nom, puis affichant le nom saisi.
+            Console.WriteLine("Entre ton nom!");
+            String nom = Console.ReadLine();
+            Console.WriteLine("bienvenue à toi " + nom);*/
+
+
+            /* Exercice 2 - Saisie d’un entier
+             Ecrire un programme demandant à l’utilisateur de saisir une valeur numérique entière puis affichant cette valeur.*/
+            /* string chiffre;
+
+             Console.WriteLine("veuillez saisir une valeur numerique");
+             chiffre= Console.ReadLine();
+             Console.WriteLine(" bien joué " + chiffre);
+
+ */
+            /*1.2 Entiers
+          Exercice 3 - Opération sur les entiers
+          Saisir deux variables entières, afficher leur somme et leur quotient.*/
+
+
+            /*string a, b;
+            int c,d;
+
+
+            Console.WriteLine("veuillez saisir une valeur numerique");
+            Console.WriteLine("Saisir un premier nombre !");
+            a = Console.ReadLine();
+            Console.WriteLine("Saisir un deuxième nombre !");
+            b = Console.ReadLine();
+            c = Convert.ToInt32(a) + Convert.ToInt32(b);
+            d = Convert.ToInt32(a) / Convert.ToInt32(b);
+            Console.WriteLine("La somme des deux nombres est égal a " + c);
+            Console.WriteLine("Le quotient des deux nombres est égal a " + d);
+*/
+
+            /*Exercice 4 - Saisie et affichage
+             Saisir une variable de type float, afficher sa valeur*/
+
+            /*String A;
+            Console.WriteLine("veuillez saisir un chiffre à virgule");
+            A = Console.ReadLine();
+            Console.WriteLine("voici votre chiffre " + A);
+*/
+
+            /*Exercice 5 - Moyenne arithmétique
+           *//* Saisir 3 valeurs, afficher leur moyenne*//*
+            string a, b, c;
+            int d;
+
+            Console.WriteLine("veuillez saisir une valeur numerique");
+            Console.WriteLine("Saisir un premier nombre !");
+            a = Console.ReadLine();
+
+            Console.WriteLine("Saisir un deuxième nombre !");
+            b = Console.ReadLine();
+            Console.WriteLine("Saisir un troisieme nombre !");
+            c = Console.ReadLine();
+
+            d = (Convert.ToInt32(a) + Convert.ToInt32(b) + Convert.ToInt32(c))/3;
+            Console.WriteLine("La somme des deux nombres est égal a " + d);
+*/
+        }
+    }
+}
