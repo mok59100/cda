42cb223851d749c51907086153103808946db664
59011-07-10 <59011-07-10@exchange.ad.afpanet>
2021-11-09 12:20:50 +0100

    09/11/2021 12:20:48,06

--- csharp/POO/Employes/Employes/Employe.cs
+++ csharp/POO/Employes/Employes/Employe.cs
@@ -11,18 +11,61 @@ namespace Employes
         // Attributs
         public string Nom { get; set; } 
         public string Prenom { get; set; }
-        public int DateEmbauche { get; set; }
+        public DateTime DateEmbauche { get; set; }
         public string Fonction { get; set; }
-        public string Salaire { get; set; }
+        public double Salaire { get; set; }
        public string Service { get; set; }
 
 
-        public Personnes(string nvNom, string nvPrenom, int nvAge, string nvAdresse)
+        public Employe(string nom, string prenom, DateTime dateEmbauche, string fonction, double salaire, string service )
         {
-            this.nom = nvNom;
-            this.prenom = nvPrenom;
-            this.age = nvAge;
-            this.adresse = nvAdresse;
+            this.Nom = nom;
+            this.Prenom = prenom;
+            this.DateEmbauche = dateEmbauche;
+            this.Fonction = fonction;
+            this.Salaire = salaire;
+            this.Service = service;
+        }
+        public int Anciennete()
+        {
+            //DateTime dateJour = DateTime.Today;
+            //DateTime dateEmb = this.DateEmbauche;
+            //TimeSpan nbJour=dateJour-dateEmb;
+            //return (int) nbJour.TotalDays/365;
+            return (int) (DateTime.Today - this.DateEmbauche).TotalDays / 365;
+        }
+
+        public double PrimeSalaireAnnuel()
+        {
+            return (this.Salaire * 5) / 100;
+        }
+
+        public double PrimeAnciennete()
+        {
+            return this.Salaire * (this.Anciennete() * 2) / 100;
+        }
+
+        public double Prime()
+        {
+            return this.PrimeSalaireAnnuel() + this.PrimeAnciennete()
+;        }
+        public override string ToString()
+        {
+            return " nom : " + this.Nom + " prenom : " + this.Prenom + " date embauche : "+ this.DateEmbauche + " fonction : "+ this.Fonction + " salaire: "+ this.Salaire + " service : "+ this.Service;
+        }
+        //public override int CompareTo (Employe e1, Employe e2)
+        //{ if (e1.Nom  e2.Nom)
+
+        //}
+        public override int CompareTo(Employe e2, Employe e1)
+        {
+            return e1.CompareTo((e2 as Employe).Nom);
+        }
+
+        private int CompareTo(string nom)
+        {
+            throw new NotImplementedException();
         }
     }
+ 
 }
--- csharp/POO/Employes/Employes/Program.cs
+++ csharp/POO/Employes/Employes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Employes
 {
@@ -6,7 +7,47 @@ namespace Employes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employe d1 = new Employe("Durand", "michel",new DateTime (2008,11,30), "directeur",49000,"developpement");
+            Employe d2 = new Employe("Dupond", "christophe", new DateTime(2007, 01, 01), "developpeur", 29200, "communication");
+            Employe d3 = new Employe("Depaix", "pierre", new DateTime(2018, 02, 10), "commercial", 33900, "strategie");
+            Employe d4 = new Employe("Deval", "sebastien", new DateTime(2012, 12, 03), "technicien", 23500, "technique");
+            Employe d5 = new Employe("Verde", "marc", new DateTime(1990, 01, 30), "poseur", 18700, "technique");
+            Console.WriteLine(d1);
+
+            double salaireAnnuel = d1.Prime();
+             Console.WriteLine(salaireAnnuel);
+            if (DateTime.Now.Month == 11 && DateTime.Now.Day ==30) 
+            {
+                Console.WriteLine("la prime sera bien versé");
+
+            }
+            else
+            {
+                Console.WriteLine("la prime sera versé bientot ");
+            }
+
+            List<Employe> ListeEmploye = new List<Employe>()
+
+            {
+                d1,
+                d2,
+                d3,
+                d4,
+                d5,
+
+            };
+            ListeEmploye.Sort();
+            Console.WriteLine();
+            foreach (Employe ListeE in ListeEmploye)
+                Console.WriteLine(ListeE);
         }
+
+
     }
-}
+
+
+
+        }
+        
+
+    
