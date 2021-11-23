f932ab8e772a672c6152d59a6fa8a0a32e79c441
mok59100 <moktar.terki@gmail.com>
2021-11-23 12:21:08 +0100

    23/11/2021 12:21:03,29

---
+++ csharp/POO/ApiPersonnes/ApiPersonnes/Data/MyDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPersonnes.Data
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext(DbContextOptions<MyDbContext> options):base(options)
+        {
+
+        }
+    }
+}
