e75bcc84f82797ec7640b967ed0e870a1b2f2eb6
mok59100 <moktar.terki@gmail.com>
2021-11-24 12:30:21 +0100

    24/11/2021 12:30:17,52

--- csharp/WebApplication1/WebApplication1/Controllers/AbonnesController.cs
+++ csharp/WebApplication1/WebApplication1/Controllers/AbonnesController.cs
@@ -7,12 +7,13 @@ using System.Threading.Tasks;
 using WebApplication1.Data.Dtos;
 using WebApplication1.Data.Models;
 using WebApplication1.Data.Services;
+using WebApplication1.Helpers;
 
 namespace WebApplication1.Controllers
 {
     [Route("api/Abonnes")]
     [ApiController]
-    public class AbonnesController
+    public class AbonnesController : ControllerBase
     {
         private readonly AbonnesServices _service;
         private readonly IMapper _mapper;
@@ -24,10 +25,41 @@ namespace WebApplication1.Controllers
 
 
         [HttpGet]
-        public IEnumerable<AbonnesDTO> test()
+        public ActionResult<IEnumerable<AbonnesDTO>> GetAllAbonnes()
         {
-            var listeabonnes = _service.GetAllAbonne();
-            return _mapper(    listeabonnes;
+            var listeabonnes = _service.GetAllAbonnes();
+            return Ok(_mapper.Map<IEnumerable<AbonnesDTO>>(listeabonnes));
+        }
+
+        //GET api/abonnes/{id}
+        [HttpGet("{id}", Name = "GetAbonneById")]
+        public ActionResult<AbonnesDTO> GetAbonneById(int id)
+        {
+            var commandItem = _service.GetAbonneById(id);
+            if (commandItem != null)
+            {
+                return Ok(_mapper.Map<AbonnesDTO>(commandItem));
+            }
+            return NotFound();
+        }
+
+        //PUT api/personnes/{id}
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateAbonne(int id, AbonnesDTO abonne)
+        {
+            var abonneFromRepo = _service.GetAbonneById(id);
+            if (abonneFromRepo == null)
+            {
+                return NotFound();
+            }
+            abonneFromRepo.Dump();
+            _mapper.Map(abonne, abonneFromRepo);
+            abonneFromRepo.Dump();
+            abonne.Dump();
+            // inutile puisque la fonction ne fait rien, mais garde la cohérence
+            _service.UpdateAbonne(abonneFromRepo);
+            return NoContent();
         }
     }
 }
--- csharp/WebApplication1/WebApplication1/Data/Services/AbonnesServices.cs
+++ csharp/WebApplication1/WebApplication1/Data/Services/AbonnesServices.cs
@@ -14,11 +14,21 @@ namespace WebApplication1.Data.Services
             _context = context;
         }
 
-        //public IEnumerable<Abonnes> GetAllAbonne()
-        public IEnumerable<Abonnes> GetAllAbonne()
+         public IEnumerable<Abonnes> GetAllAbonnes()
         {
             
             return _context.Abonnes.ToList();
+        }
+
+        public Abonnes GetAbonneById(int id)
+        {
+            return _context.Abonnes.FirstOrDefault(p => p.IdAbonne == id);
+        }
+        public void UpdateAbonne(Abonnes p)
+        {
+            //nothing
+            //on va mettre à jour le context dans le controller par mapping et passer 
+            //les modifs à la base
         }
     }
 }
