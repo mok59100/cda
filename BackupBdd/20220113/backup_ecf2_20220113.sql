94b812c4017cc6d35acf9e3b7d0a1d6bdbac7829
mok59100 <moktar.terki@gmail.com>
2022-01-14 08:27:03 +0100

    base

---
+++ BackupBdd/20220113/backup_ecf2_20220113.sql
@@ -0,0 +1,91 @@
+-- MySQL dump 10.13  Distrib 5.7.28, for Win32 (AMD64)
+--
+-- Host: localhost    Database: ecf2
+-- ------------------------------------------------------
+-- Server version	5.7.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `ecf2`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `ecf2` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `ecf2`;
+
+--
+-- Table structure for table `chambres`
+--
+
+DROP TABLE IF EXISTS `chambres`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `chambres` (
+  `idChambre` int(11) NOT NULL AUTO_INCREMENT,
+  `typeChambre` int(11) NOT NULL,
+  `capaciteChambre` int(11) NOT NULL,
+  `numChambre` int(11) NOT NULL,
+  `idHotel` int(11) NOT NULL,
+  PRIMARY KEY (`idChambre`),
+  KEY `FK_chambres_hotels` (`idHotel`),
+  CONSTRAINT `FK_chambres_hotels` FOREIGN KEY (`idHotel`) REFERENCES `hotels` (`idHotel`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chambres`
+--
+
+LOCK TABLES `chambres` WRITE;
+/*!40000 ALTER TABLE `chambres` DISABLE KEYS */;
+INSERT INTO `chambres` VALUES (1,1,1,101,1),(2,1,2,102,1),(3,1,1,103,1),(4,1,2,104,2),(5,1,2,105,2),(7,1,3,107,3),(8,1,1,108,3),(9,1,3,109,2);
+/*!40000 ALTER TABLE `chambres` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hotels`
+--
+
+DROP TABLE IF EXISTS `hotels`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hotels` (
+  `idHotel` int(11) NOT NULL AUTO_INCREMENT,
+  `nomHotel` varchar(50) NOT NULL,
+  `categorieHotel` int(11) NOT NULL,
+  `adresseHotel` varchar(50) NOT NULL,
+  `villeHotel` varchar(50) NOT NULL,
+  PRIMARY KEY (`idHotel`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hotels`
+--
+
+LOCK TABLES `hotels` WRITE;
+/*!40000 ALTER TABLE `hotels` DISABLE KEYS */;
+INSERT INTO `hotels` VALUES (1,'Le Magnifique',3,'rue du bas','Pralo'),(2,'Hotel du haut',1,'rue du haut','Pralo'),(3,'Le Narval',3,'place de la liberation','Vonten'),(4,'Les Pissenlis',4,'place du 14 juillet','Bretou');
+/*!40000 ALTER TABLE `hotels` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-01-13 21:48:48
